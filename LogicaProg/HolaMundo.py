d838bce2cf9b94f2bec3517d62cfb72ababcc7eb
Mar <66106505+Marvoo200@users.noreply.github.com>
2022-05-02 19:49:33 -0500

    Primer actualización

---
+++ LogicaProg/HolaMundo.py
@@ -0,0 +1,11 @@
+#Comentario: Ejercicio de prints
+#Los comentarios sirven para documentar el código o programa 
+#y los omite o no los toma en cuenta python
+
+
+#imprimir o mostrar la salida en pantalla
+print("Hola mundo")
+print('Marvin Nahum Toribio Cortes')
+print(5+5)
+
+print("Curso")
