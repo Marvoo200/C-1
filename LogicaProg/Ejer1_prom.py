384d49eac48e48d1db18fd898b5701e158f7d2ba
Mar <66106505+Marvoo200@users.noreply.github.com>
2022-05-04 19:19:14 -0500

    Clase 3

---
+++ LogicaProg/Ejer1_prom.py
@@ -0,0 +1,15 @@
+#entradas
+
+cal1 = float(input("Ingrese la calificacion 1: "))
+cal2 = float(input("Ingrese la calificacion 1: "))
+cal3 = float(input("Ingrese la calificacion 1: "))
+
+#proceso
+
+promedio = (cal1 + cal2 + cal3)/3
+
+
+
+#Salida
+
+print("El promedio es: ", round(promedio, 2))
