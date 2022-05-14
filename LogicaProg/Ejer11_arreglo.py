e072c87ed8f1fb3b9cfd5d9dd365401358ddca25
Mar <66106505+Marvoo200@users.noreply.github.com>
2022-05-13 20:05:57 -0500

    Clase 8

---
+++ LogicaProg/Ejer11_arreglo.py
@@ -0,0 +1,16 @@
+#hacer un arreglo de 10 elementos y los imprima
+
+#Entradas
+arreglo =["Jacob", "Martin", "Ximena", "Eduardo", "Omar", "Ricardo", 5, 15, 25.3, 12.2]
+arreglo1 = [2, 5, 6, 8, 10, 8, 7, 11.5, 12.8, 13.5, 14, 13]
+print(arreglo)
+print(arreglo1)
+#Procesos
+arreglo.append("Carla")
+arreglo.append(False)
+arreglo1.sort()
+
+#Salidas
+
+print(arreglo)
+print(arreglo1)
---
+++ LogicaProg/Ejer8_numeros.py
@@ -0,0 +1,31 @@
+'''
+Obtener un número y determinar lo siguiente:
+a) si es negativo y mayor a -100 imprimir los números impares de forma DESCENDENTE
+b) si es positivo y menor a 100 mostrar los números pares de forma ASCENDENTE
+c) en otro caso imprimir No Válido
+'''
+
+
+#entradas
+
+num = int(input("Ingresa el numero: "))
+
+#Proceso
+
+if(num <= 100 and num > 0):
+    for i in range(1, num):
+        if(i %2 == 0):
+            print(i)
+elif(num<0 and num >=-100):
+    for i in range (0, num, -1):
+        if (i %2 !=0 and num < 0):
+            print(i)
+else:
+    print("Numero invalido, solo se puede de -100 a 100")
+
+#Salidas
+
+    
+    
+    
+
